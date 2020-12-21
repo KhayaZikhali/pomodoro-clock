7185e2e389d7bd177fe2a0034d098bcce0bc3567
KhayaZikhali <70526289+KhayaZikhali@users.noreply.github.com>
2020-12-21 19:27:27 +0100

    something

--- src/clock.js
+++ src/clock.js
@@ -5,45 +5,52 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // create the basic layout of the clock
 // the buttons 
 // the divs that will have the time within them
+// add button space to start and stop
 
 class Clockface extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
           breakLength: 0 ,
-          sessionLength : 25 ,
+          sessionLength : 25,
+          time :{},
+          seconds : 0
         };
     // increment binders 
        this.increment2 = this.incrementSessionLength.bind(this);
        this.increment = this.incrementBreakLength.bind(this);
        this.decrement2 = this.decrementSessionLength.bind(this);
        this.decrement = this.decrementBreakLength.bind(this);
-      }
-     // increment the break length 
+      };     
+    // increment the break length 
         incrementBreakLength(){
         this.setState ( state=>{
         return {breakLength : state.breakLength += 1}
          }
-        )}
-     // decrement the break length
+        )};
+    // decrement the break length
       decrementBreakLength(){
         this.setState ( state=>{
         return { breakLength : state.breakLength -= 1}
          })
-        }
+      };
 
     // increment the session length     
         incrementSessionLength(){
          this.setState ( state=>{
         return {sessionLength : state.sessionLength += 1}
          })
-        ;}
+        };
     // decrement the session     
         decrementSessionLength(){
          this.setState ( state=>{
         return { sessionLength : state.sessionLength -= 1}
          }
-        )}; 
+        )};
+
+
+    // add a a function to start the countdown    
+
         render() {
             // three seperate divs
             // one for the break length
@@ -55,7 +62,7 @@ class Clockface extends React.Component {
              <div className="breakLength"> 
              Break Length
              <div>
-             <button className="btn-up btn arrow this" id="breakincrease" onClick={this.increment}> + </button>
+             <button className="btn arrow this" id="breakincrease" onClick={this.increment}><i class="fas fa-angle-double-up"></i> + </button>
                 {this.state.breakLength}
              <button className="btn-up btn arrow" id="breakdecrease" onClick={this.decrement}> - </button>
              </div>
@@ -71,7 +78,7 @@ class Clockface extends React.Component {
              </div>
              <div className="timer"> ACTUAL TIMER
              <div>
-                 {this.state.sessionLength}
+                 {this.state.sessionLength} : {this.state.seconds}
              </div>
               </div>
             </div>
