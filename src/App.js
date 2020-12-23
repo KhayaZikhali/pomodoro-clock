05bffe7ce3c5a3f7a594ec44c9bad7ce5ae9d58b
KhayaZikhali <70526289+KhayaZikhali@users.noreply.github.com>
2020-12-23 20:27:00 +0100

    removed most of the code cause im starting it all over

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react'
 import './App.css';
-import {Clockface} from './clock.js';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import {Clock} from './clock.js'
 
-// add clock face 
-// add counter
-// add play/pause  button 
 
 function App() {
   return (
     <div className="App">
-      <Clockface />
+      <Clock />
     </div>
   );
 }
