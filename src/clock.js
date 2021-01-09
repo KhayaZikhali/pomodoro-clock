2fa0ed46b3d793fc3faf87abb0797e6729e57023
KhayaZikhali <70526289+KhayaZikhali@users.noreply.github.com>
2021-01-09 22:00:11 +0100

    something

--- src/clock.js
+++ src/clock.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css'
 
-
 class Clockface extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
           breakLength: 5 ,
           sessionLength : 25 ,
-          seconds : 0,
+          seconds: 0, 
+          minutes: 0
         };
 
     // increment binders 
@@ -17,8 +17,9 @@ class Clockface extends React.Component {
        this.decrement2 = this.decrementSessionLength.bind(this);
        this.decrement = this.decrementBreakLength.bind(this);
        this.resetCounter = this.resetCounter.bind(this)
-       this.begin = this.begin.bind(this)
+       this.start = this.start.bind(this)
       }
+
      // increment the break length 
         incrementBreakLength(){
          if(this.state.breakLength < 60){  
@@ -51,31 +52,47 @@ class Clockface extends React.Component {
              })
           }};
 
-     //reset Counter 
+     // reset Counter 
      resetCounter(){
+        setTimeout(this.start() ,10)
+        this.start(false)
+        document.querySelector(".time").innerHTML = (this.state.sessionLength+":00" )
        this.setState(state => {
          return  {breakLength : 5 ,
                   sessionLength : 25 
        }
      });
-    }
+    };
 
-      begin() {
-      var minute = this.state.sessionLength;
-      var sec = this.state.seconds;
-      setInterval(function() {
-         sec = sec - 1;
-        if (sec === 0) {
-         minute =  minute - 1;
-          this.setState( state => {
-            return  sec = 59
-         })
-        if (minute === 0) {
-              return;
-          }
-        }
-      }, 10);
-    }
+// logic to handle the timer countdown 
+ startTimer(duration, display){
+  var timer = duration, minutes, seconds;
+  setInterval(function () {
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+      display.textContent = minutes + ":" + seconds;
+      if (--timer < 0) {
+          timer = duration;
+      }
+      else if(timer === 0){
+        return;
+      }
+  }, 1000)}
+
+// function to start the coutdown when the start button is clicked
+ start(run){document.querySelector("#btnstart").addEventListener("click",()=>{
+  var fiveMinutes = 60 * this.state.sessionLength,
+      display = document.querySelector('.time');
+      var myInterval = run
+      if(myInterval === true){
+     this.startTimer(fiveMinutes, display);
+      } else{
+        return;
+      }
+    });
+}
 
         render() {
            return (
@@ -96,7 +113,7 @@ class Clockface extends React.Component {
                 Session Length
              <div>
              <button className="btns btn bi-arrow-up" id="sessPlus" onClick={this.increment2}><b>&#x2191;</b></button>
-                <p>{this.state.sessionLength}</p>
+                <p id="splay">{this.state.sessionLength}</p>
              <button  className="btns btn bi-arrow-down" id="sessMinus" onClick={this.decrement2}><b>&#x2193;</b></button>
              </div>
              </div>
@@ -104,11 +121,11 @@ class Clockface extends React.Component {
             {/* TIMER */} 
              <div className="timer"> 
               <div className="time">
-                 {this.state.sessionLength}:{this.state.seconds}
-              </div>
+                 {this.state.sessionLength}:00
+               </div>
                 <div class="play-button-outer">
-                 <button className="Play btn btn-primary" id="btnstart" onClick={this.begin}>Start</button>          
-                 <button className="Pause btn btn-secondary" id="btnpause">Pause</button>          
+                 <button className="Play btn btn-success" id="btnstart" onClick={()=> this.start(false)}>Start</button>          
+                 <button className="Pause btn btn-primary" id="btnpause">Pause</button>          
                  <button className="Reset btn btn-danger"  id="btnreset" onClick={this.resetCounter}>Reset</button>          
               </div>
               </div>
